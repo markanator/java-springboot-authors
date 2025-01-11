b6afc9d3734f23897d169324973d352ea188a2cb
Mark Ambro <mark.ambro@me.com>
2025-01-11 14:56:29 -0700

    custom queries

--- src/main/java/com/ambro/authors/repositories/IAuthorRepository.java
+++ src/main/java/com/ambro/authors/repositories/IAuthorRepository.java
@@ -1,9 +1,15 @@
 package com.ambro.authors.repositories;
 
 import com.ambro.authors.domain.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAuthorRepository extends CrudRepository<Author, Long> {
+    // JPA will auto generate the sql using the name of the method
+    Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a from Author a where a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
 }
--- src/test/java/com/ambro/authors/repositories/AuthorRepositoryIntegrationTest.java
+++ src/test/java/com/ambro/authors/repositories/AuthorRepositoryIntegrationTest.java
@@ -80,4 +80,32 @@ public class AuthorRepositoryIntegrationTest {
         Optional<Author> result = underTest.findById(authorA.getId());
         assertThat(result).isEmpty();
     }
+
+    @Test
+    public void testThatGetAuthorsWithAgeLessThan(){
+        Author authorA = TestDataUtil.createTestAuthorA();
+        underTest.save(authorA);
+        Author authorB = TestDataUtil.createTestAuthorB();
+        underTest.save(authorB);
+        Author authorC = TestDataUtil.createTestAuthorC();
+        underTest.save(authorC);
+
+        Iterable<Author> result = underTest.ageLessThan(50);
+        assertThat(result)
+                .containsExactly(authorB, authorC);
+    }
+
+    @Test
+    public void testThatGetAuthorsWithAgeGreaterThan(){
+        Author authorA = TestDataUtil.createTestAuthorA();
+        underTest.save(authorA);
+        Author authorB = TestDataUtil.createTestAuthorB();
+        underTest.save(authorB);
+        Author authorC = TestDataUtil.createTestAuthorC();
+        underTest.save(authorC);
+
+        Iterable<Author> result = underTest.findAuthorsWithAgeGreaterThan(50);
+        assertThat(result)
+                .containsExactly(authorA);
+    }
 }
